327a060b687a93fae97de1f100aa2404d4c841fb
maliang04 <maliang04@meituan.com>
2017-01-19 15:16:34 +0800

    add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,8 +4,8 @@ describe "Static pages" do
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
-		      visit '/static_pages/home'
-		      expect(page).to have_content('Sample App')
+		    visit '/static_pages/home'
+		    expect(page).to have_content('Sample App')
 		end 
 	end
 end
